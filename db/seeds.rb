c4f3470248982c8ef8a158fb541b5a64fc2b83e6
Alexandro <alejo_alvarez67@hotmail.com>
2018-04-30 15:37:09 +0200

    new

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+#Movie.all.destroy
+
+
+
+Movie.create([{title: 'Fight club', release_year: 1999, director: 'David Fincher'}, {title: 'Nosso Lar', release_year: 1944, director: 'Chico Xavier'},
+    {title:'Battle Royale', release_year: 2000, director: 'Takeshi Kitano'}])
